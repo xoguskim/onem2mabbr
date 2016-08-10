13d2539566eef2242304f0aced435ff63a1d335f
Taehyun Kim <taehyun.kim@daouincube.com>
2016-08-10 15:05:20 +0900

    revice UI

--- public/javascripts/ngui.abbr.js
+++ public/javascripts/ngui.abbr.js
@@ -2,11 +2,12 @@
 var onem2mAbbr = angular.module('onem2mAbbr', []);
 onem2mAbbr.controller('mainController', mainController);
 
+var apiPrefix = '/onem2mdoc';
 function mainController($scope, $http) {
   $scope.formData = {};
 
   // when landing on the page, get all todos and show them
-  $http.get('/onem2mdoc/api/abbr')
+  $http.get(apiPrefix + '/api/abbr')
     .success(function(data) {
       $scope.abbrList = data;
       console.log(data);
@@ -17,7 +18,7 @@ function mainController($scope, $http) {
 
   // when submitting the add form, send the text to the node API
   $scope.putWord = function() {
-    $http.put('/onem2mdoc/api/abbr', $scope.formData)
+    $http.put(apiPrefix + '/api/abbr', $scope.formData)
       .success(function(data) {
         $scope.formData = {}; // clear the form so our user is ready to enter another
         $scope.abbrList = data;
@@ -30,7 +31,7 @@ function mainController($scope, $http) {
 
   // delete a todo after checking it
   $scope.deleteAbbr = function(word) {
-    $http.delete('/onem2mdoc/api/abbr/' + word)
+    $http.delete(apiPrefix + '/api/abbr/' + word)
       .success(function(data) {
         $scope.abbrList = data;
         console.log(data);
